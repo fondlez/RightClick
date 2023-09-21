288ffbc9075551a20b2be999d1e62bafd32efb69
vortigern11 <darkarchan@abv.bg>
2023-09-21 10:36:53 +0300

    better conditional

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # RightClick Addon for WoW
 Disables right click attack on enemy players. \
-Disables right click targeting. \
-In order to interact, you will have to target with left click first!
+Disables right click targeting if an enemy is selected.
--- RightClick.lua
+++ RightClick.lua
@@ -1,20 +1,24 @@
-local tt = CreateFrame('Frame', nil, GameTooltipStatusBar)
+local tt = CreateFrame("Frame", nil, GameTooltipStatusBar)
+
 tt:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 tt:SetScript("OnEvent", function()
-    local playerFaction = UnitFactionGroup("player")
-    local mouseoverFaction = UnitFactionGroup("mouseover")
+    if event == "UPDATE_MOUSEOVER_UNIT" then
+        local playerFaction = UnitFactionGroup("player")
+        local mouseoverFaction = UnitFactionGroup("mouseover")
 
-    this.name = UnitName("mouseover")
-    this.isPlayer = UnitIsPlayer("mouseover")
-    this.isEnemyFaction = mouseoverFaction ~= playerFaction
+        this.name = UnitName("mouseover")
+        this.isPlayer = UnitIsPlayer("mouseover")
+        this.isEnemyFaction = mouseoverFaction ~= playerFaction
+    end
 end)
 
 WorldFrame:SetScript("OnMouseUp", function()
     if arg1 == "RightButton" then
+        local hasEnemyTarget = UnitExists("target") and not UnitIsDeadOrGhost("target") and not UnitIsFriend("player", "target")
         local isNewTarget = UnitName("target") ~= tt.name
         local isEnemyPlayer = tt.isPlayer and tt.isEnemyFaction
 
-        if isNewTarget or isEnemyPlayer then
+        if hasEnemyTarget and (isNewTarget or isEnemyPlayer) then
             MouselookStop()
             return
         end
