97e84c53ddb1d7ca90f40b867cf753d752bf67d8
vortigern11 <darkarchan@abv.bg>
2023-09-23 08:54:40 +0300

    in combat check

--- RightClick.lua
+++ RightClick.lua
@@ -1,6 +1,7 @@
 local tt = CreateFrame("Frame", nil, GameTooltipStatusBar)
 
 tt:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+
 tt:SetScript("OnEvent", function()
     if event == "UPDATE_MOUSEOVER_UNIT" then
         local playerFaction = UnitFactionGroup("player")
@@ -15,10 +16,11 @@ end)
 WorldFrame:SetScript("OnMouseUp", function()
     if arg1 == "RightButton" then
         local hasEnemyTarget = UnitExists("target") and not UnitIsDeadOrGhost("target") and not UnitIsFriend("player", "target")
+        local inCombat = UnitAffectingCombat("player")
         local isNewTarget = UnitName("target") ~= tt.name
         local isEnemyPlayer = tt.isPlayer and tt.isEnemyFaction
 
-        if hasEnemyTarget and (isNewTarget or isEnemyPlayer) then
+        if inCombat and hasEnemyTarget and (isNewTarget or isEnemyPlayer) then
             MouselookStop()
             return
         end
