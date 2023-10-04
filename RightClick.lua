865be915f4f871ec3c92b1478ff17593400b0ae7
vortigern11 <darkarchan@abv.bg>
2023-10-04 13:03:03 +0300

    fixed all cases

--- RightClick.lua
+++ RightClick.lua
@@ -1,28 +1,54 @@
-local tt = CreateFrame("Frame", nil, GameTooltipStatusBar)
+local mouseInfo = CreateFrame("Frame", nil, GameTooltipStatusBar)
 
-tt:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+mouseInfo:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 
-tt:SetScript("OnEvent", function()
+mouseInfo.time = 0
+mouseInfo.isEnemy = false
+mouseInfo.isAlive = false
+
+mouseInfo:SetScript("OnEvent", function()
     if event == "UPDATE_MOUSEOVER_UNIT" then
         local playerFaction = UnitFactionGroup("player")
         local mouseoverFaction = UnitFactionGroup("mouseover")
+        local exists = UnitExists("mouseover")
 
-        this.name = UnitName("mouseover")
-        this.isPlayer = UnitIsPlayer("mouseover")
-        this.isEnemyFaction = mouseoverFaction ~= playerFaction
+        if exists then
+            this.time = GetTime()
+            this.isEnemy = not UnitIsFriend("player", "mouseover")
+            this.isAlive = not UnitIsDeadOrGhost("mouseover")
+            -- this.name = UnitName("mouseover")
+            -- this.isEnemyPlayer = UnitIsPlayer("mouseover") and mouseoverFaction ~= playerFaction
+        end
     end
 end)
 
 WorldFrame:SetScript("OnMouseUp", function()
     if arg1 == "RightButton" then
-        local hasEnemyTarget = UnitExists("target") and not UnitIsDeadOrGhost("target") and not UnitIsFriend("player", "target")
-        local inCombat = UnitAffectingCombat("player")
-        local isNewTarget = UnitName("target") ~= tt.name
-        local isEnemyPlayer = tt.isPlayer and tt.isEnemyFaction
+        local now = GetTime()
+        local mouseoverWasNow = (now - mouseInfo.time) <= 1
+        local hasTarget = UnitExists("target")
 
-        if inCombat and hasEnemyTarget and (isNewTarget or isEnemyPlayer) then
-            MouselookStop()
-            return
+        -- let stuff happen if not about to click on a current mouseover unit
+        if not mouseoverWasNow then return end
+
+        local shouldPrevent = not hasTarget and mouseInfo.isAlive and mouseInfo.isEnemy
+
+        -- | target | mouseover | what |
+        -- | -      | -         | -    |
+        -- | f      | f         | N    |
+        -- | f      | e         | P    |
+        -- | f      | nil       | N    |
+        -- | e      | f         | P    |
+        -- | e      | e         | P    |
+        -- | e      | nil       | N    |
+
+        if not shouldPrevent then
+            local targetIsAliveEnemy = UnitExists("target") and not UnitIsDeadOrGhost("target") and not UnitIsFriend("player", "target")
+            local inCombat = UnitAffectingCombat("player") or UnitAffectingCombat("target")
+
+            shouldPrevent = hasTarget and (inCombat or targetIsAliveEnemy or mouseInfo.isEnemy)
         end
+
+        if shouldPrevent then MouselookStop() end
     end
 end)
