8cb175159982365b6f80d74ada6b585e4aa4a68b
fondlez <fondlez@protonmail.com>
2024-02-04 14:44:28 +0000

    Optimize and test across vanilla to MoP expansions
    Applies minor optimizations and tested to work across vanilla (1.12),
    TBC (2.4.3), WotLK (3.3.5a), Cata (4.3.4) and MoP (5.4.8) clients.

--- RightClick.lua
+++ RightClick.lua
@@ -1,30 +1,47 @@
-local mouseInfo = CreateFrame("Frame", nil, GameTooltipStatusBar)
+local _, _, _, client = GetBuildInfo()
+client = client or 11200
+
+local GetTime = GetTime
+local MouselookStop = MouselookStop
+local UnitCanAttack = UnitCanAttack
+local UnitExists = UnitExists
+local UnitIsDeadOrGhost = UnitIsDeadOrGhost
+
+local mouseInfo = CreateFrame("Frame")
 
 mouseInfo:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 
 mouseInfo.time = 0
 mouseInfo.isAliveEnemy = false
 
-mouseInfo:SetScript("OnEvent", function()
-    if event == "UPDATE_MOUSEOVER_UNIT" then
-        local playerFaction = UnitFactionGroup("player")
-        local mouseoverFaction = UnitFactionGroup("mouseover")
-        local exists = UnitExists("mouseover")
-
-        if exists then
-            this.time = GetTime()
-            this.isAliveEnemy = not UnitIsDeadOrGhost("mouseover") and not UnitIsFriend("player", "mouseover") and not UnitIsCivilian("mouseover")
-            -- this.isEnemyPlayer = UnitIsPlayer("mouseover") and mouseoverFaction ~= playerFaction
-        end
-    end
+mouseInfo:SetScript("OnEvent", function(self, e)
+  local this = self or this
+  local event = e or event
+  
+  if event == "UPDATE_MOUSEOVER_UNIT" then
+    this.time = GetTime()
+    this.isAliveEnemy = not UnitIsDeadOrGhost("mouseover") 
+      and UnitCanAttack("player", "mouseover")
+  end
 end)
 
-WorldFrame:SetScript("OnMouseUp", function()
-    if arg1 == "RightButton" then
-        local mouseoverWasNow = (GetTime() - mouseInfo.time) <= 5
-        local targetIsAliveEnemy = UnitExists("target") and not UnitIsDeadOrGhost("target") and not UnitIsFriend("player", "target") and not UnitIsCivilian("mouseover")
-        local shouldPrevent = (mouseoverWasNow and mouseInfo.isAliveEnemy) or targetIsAliveEnemy
+local OnMouseUp = function(self, button)
+  local arg1 = button or arg1
+  
+  if arg1 == "RightButton" then
+    if (GetTime() - mouseInfo.time) > 5 then return end
+    
+    local targetIsAliveEnemy = UnitExists("target") 
+      and not UnitIsDeadOrGhost("target") and UnitCanAttack("player", "target")
+    local shouldPrevent = mouseInfo.isAliveEnemy or targetIsAliveEnemy
 
-        if shouldPrevent then MouselookStop() end
-    end
-end)
+    if shouldPrevent then MouselookStop() end
+  end
+end
+
+-- HookScript does not exist before TBC
+if client >= 20000 then
+  WorldFrame:HookScript("OnMouseUp", OnMouseUp)
+else
+  WorldFrame:SetScript("OnMouseUp", OnMouseUp)
+end
